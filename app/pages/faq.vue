3be5058e6416b8a69173386bfe48e8ffdac551fc
Soraia <sorilc@hotmail.com>
2025-12-24 18:40:30 +0100

    size

--- app/pages/faq.vue
+++ app/pages/faq.vue
@@ -46,7 +46,7 @@ const ctaLinks = [
       title="Frequently Asked Questions"
       description="Everything you need to know about MiroMiro. Can't find an answer? Contact our support team."
     >
-      <div class="min-w-3xl mx-auto">
+      <div class="w-xs lg:w-xl xl:w-3xl mx-auto">
         <UAccordion :items="faqItems" />
       </div>
     </UPageSection>
